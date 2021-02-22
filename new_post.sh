81e0ae4d5208a1cb85cb00f3b4c7db0d18274594
Aleksandr Beliaev <trap000d@gmail.com>
2021-02-22 14:16:40 +1300

    Script for generating new posts

---
+++ new_post.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# Creates an empty markdown post file in "_posts" directory
+# and adds there front-matter with current time
+
+usage (){
+    echo    "Usage: $0 [New Post Title]"
+}
+
+while getopts ":h" option; do
+   case $option in
+      h)
+         usage
+         exit;;
+     \?) 
+         echo "Error: Invalid option"
+         usage
+         exit;;
+   esac
+done
+
+TITLE=$1
+
+if [ ! $TITLE ]; then
+  TITLE="No Title"
+fi
+
+title="${TITLE,,}"
+postfile="./_posts/$(date +%F)-${title// /-}.markdown"
+postdate=$(date +"%F %H:%M:%S %z")
+
+if [ ! -f "$postfile" ]; then
+echo "---
+layout: post
+published: false
+title:  $TITLE
+date:   $postdate
+categories: none
+---" > $postfile
+else
+  echo "Post with the same title already exists, please give it other name"
+fi
